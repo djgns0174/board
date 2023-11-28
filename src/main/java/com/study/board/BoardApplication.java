bf948a1f779a1814182311aa903a3330c96cd2d1
djgns0174 <74578132+djgns0174@users.noreply.github.com>
2023-11-28 20:07:39 +0900

    assignment1

--- src/main/java/com/study/board/BoardApplication.java
+++ src/main/java/com/study/board/BoardApplication.java
@@ -2,7 +2,7 @@ package com.study.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//asdfasdf
 @SpringBootApplication
 public class BoardApplication {
 
